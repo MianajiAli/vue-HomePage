5ff354cc1d79ec2cc5af2f1bbd6c9aff0523268e
ali <alisarabi304@gmail.com>
2024-06-29 23:10:18 +0330

    .

--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -1,7 +1,7 @@
 <template>
     <div class=" w-[18rem] min-h-[16rem] bg-background text-primary rounded-xl pb-3">
         <div>
-            <img class="w-full h-[8rem] p-2 rounded-2xl object-cover " src="https://picsum.photos/300/200" alt="">
+            <img class="w-full h-[8rem] p-2 rounded-2xl object-cover " src="/images/296-300x200.jpg" alt="">
         </div>
         <div class="w-full px-4">
             <span class="text-lg">Title</span>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,7 +1,7 @@
 <template>
     <div class=" w-[16rem] min-h-[16rem] bg-secondary text-primary rounded-xl pb-3">
         <div>
-            <img class="w-full h-[8rem] p-2 rounded-2xl object-cover " src="https://picsum.photos/300/200" alt="">
+            <img class="w-full h-[8rem] p-2 rounded-2xl object-cover " src="/images/296-300x200.jpg" alt="">
         </div>
         <div class="w-full min-h-[4rem] px-4 flex flex-col justify-between gap-2 ">
             <span>Title</span>
